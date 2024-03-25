f75f8a1a6f9354bba013b9ea18c55452fad0a930
ToanICV <toandv.icv@gmail.com>
2024-03-25 16:21:28 +0700

    .

--- TwoStreamNetwork/training.py
+++ TwoStreamNetwork/training.py
@@ -167,6 +167,7 @@ if __name__ == "__main__":
         scheduler.step()
         for step, batch in enumerate(train_dataloader):
             model.module.set_train()
+            print(f"model: {model}")
             output = model(is_train=True, **batch)
 
             with torch.autograd.set_detect_anomaly(True):           
